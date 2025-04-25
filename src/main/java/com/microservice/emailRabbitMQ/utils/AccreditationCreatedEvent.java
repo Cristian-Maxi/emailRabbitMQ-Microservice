1eba7070c5424da28d6ac7132dfdf0ce59f11ba5
Cristian-Maxi <cristiangomez0686@outlook.com>
2025-04-25 13:43:35 -0300

    Fix swagger config, fix tests y app properties

--- src/main/java/com/microservice/emailRabbitMQ/utils/AccreditationCreatedEvent.java
+++ src/main/java/com/microservice/emailRabbitMQ/utils/AccreditationCreatedEvent.java
@@ -4,7 +4,6 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AccreditationCreatedEvent {
@@ -14,4 +13,52 @@ public class AccreditationCreatedEvent {
     private Long pointOfSaleId;
     private String pointOfSaleName;
     private LocalDateTime receivedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getReceivedAt() {
+        return receivedAt;
+    }
+
+    public void setReceivedAt(LocalDateTime receivedAt) {
+        this.receivedAt = receivedAt;
+    }
+
+    public String getPointOfSaleName() {
+        return pointOfSaleName;
+    }
+
+    public void setPointOfSaleName(String pointOfSaleName) {
+        this.pointOfSaleName = pointOfSaleName;
+    }
+
+    public Long getPointOfSaleId() {
+        return pointOfSaleId;
+    }
+
+    public void setPointOfSaleId(Long pointOfSaleId) {
+        this.pointOfSaleId = pointOfSaleId;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/main/java/com/microservice/emailRabbitMQ/utils/UserRegisteredEvent.java
+++ src/main/java/com/microservice/emailRabbitMQ/utils/UserRegisteredEvent.java
@@ -2,11 +2,34 @@ package com.microservice.emailRabbitMQ.utils;
 
 import lombok.*;
 
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserRegisteredEvent {
     private String email;
     private String name;
     private String lastname;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
